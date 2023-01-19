89c744f74e6a3620a263aa179bf161c66fcf81fc
Joao Paulo L Silva <lsijoa02@CSGICORP.COM>
2023-01-19 11:16:57 -0300

    Criando um botão

--- alura-studies/src/App.tsx
+++ alura-studies/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Botao from './components/Botao'
+
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <Botao />
     </div>
   );
 }

af9b739ad55a0eae2ac2d3d21e7afba61ff91ad6
joaumsud <joaumsud@hotmail.com>
2023-02-05 20:07:29 -0300

    Adicionado const Botao e atributo titulo

--- alura-studies/src/components/Botao/index.tsx
+++ alura-studies/src/components/Botao/index.tsx
@@ -1,22 +1,11 @@
-import React, { Children } from 'react';
+import React from 'react';
 import styles from './Botao.module.scss';
 
 
-class Botao extends React.Component{
-    render() {
-        return(
-            <button className={styles.botao}>{children}</button>
-        )
-    }
+const Botao = (props: any) =>{
+ return (
+    <button className={styles.botao}> {props.titulo}</button>
+ )
 }
 
-
-
-/*
-const Botao = ({children, ...props }) => {
-    return (
-    <button className={styles.botao}>{children}</button>
-    )
-}
-*/
 export default Botao;
